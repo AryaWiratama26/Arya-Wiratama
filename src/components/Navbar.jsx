c0e78acc9fa7226dd0a65213b5eb8640c1a1b6f0
Arya Wiratama <aryawiratama2401@gmail.com>
2025-02-20 20:41:06 +0700

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+    return (
+        <header>
+            <div>
+                <h1>Arya Wiratama</h1>
+            </div>
+
+            <nav>
+                <ul>
+                    <li><a href="">About Me</a></li>
+                    <li><a href="">My Project</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
